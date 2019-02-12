071b3a84b921adf39e2446f609ddc070d93c40be
Joe Johans <lgvichy@gmail.com>
2019-02-11 20:32:42 -0800

    Updates

---
+++ precache-manifest.cb118b546ab985917136075f9d42c06e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e962d312b0328d9d21b",
+    "url": "/nemo/static/js/runtime~main.8e962d31.js"
+  },
+  {
+    "revision": "76a89332d987f9c6d6d9",
+    "url": "/nemo/static/js/main.76a89332.chunk.js"
+  },
+  {
+    "revision": "4c8f2e9d257217d06b0f",
+    "url": "/nemo/static/js/2.4c8f2e9d.chunk.js"
+  },
+  {
+    "revision": "76a89332d987f9c6d6d9",
+    "url": "/nemo/static/css/main.0c0686f1.chunk.css"
+  },
+  {
+    "revision": "7ae286471e94065458aefca14b9b311c",
+    "url": "/nemo/index.html"
+  }
+];
