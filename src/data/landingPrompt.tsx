3e8b99dbfbdc6f78d9c58c8d27dd260f0cb07e9c
Joe Johans <lgvichy@gmail.com>
2019-02-28 13:16:23 -0800

    Refactor and ready to integrate the init profile flow

---
+++ src/data/landingPrompt.tsx
@@ -0,0 +1,54 @@
+import { createOptionLabel, appendTrigger } from "../core/utils";
+import { LandingDataTable } from "../components/LandingDataCard";
+import { strings } from "../i18n";
+
+export const landingPromptOrder = [
+  "prompt_landing",
+  "add_landingDisposition",
+  "prompt_landingConfirm",
+  "prompt_confirmData",
+  "add_confirmData",
+  "prompt_checkConfirm",
+  "prompt_anotherOne"
+];
+
+export const createLandingPrompt = () =>
+  [
+    {
+      id: `prompt_landing`,
+      hideInput: true
+    },
+    {
+      id: `add_landingDisposition`,
+      options: ["inprogress", "finished"].map(createOptionLabel)
+    },
+    // { // TODO: Stretch, ask cargo load
+    //   id: `prompt_cargoLoad`,
+    //   hideInput: true,
+    //   message: ({ previousValue }: any) =>
+    //     previousValue === "finished"
+    //       ? strings.prompt_cargoLoad
+    //       : "✔"
+    // },
+    {
+      id: `prompt_landingConfirm`,
+      component: <LandingDataTable />
+    },
+    {
+      id: `prompt_confirmData`,
+      hideInput: true
+    },
+    {
+      id: `add_confirmData`,
+      hideInput: true,
+      options: ["dataIsAccurate", "dataIsInaccurate"].map(createOptionLabel)
+    },
+    {
+      id: `prompt_checkConfirm`,
+      hideInput: true,
+      message: ({ previousValue }: any) =>
+        previousValue === "dataIsInaccurate"
+          ? strings.prompt_errorReported
+          : "✔"
+    }
+  ].map((p: any, i) => appendTrigger(p, i, landingPromptOrder));
---
+++ src/data/salePrompt.tsx
@@ -0,0 +1,59 @@
+import { CatchSelector } from "../components/CatchSelector";
+import { numberValidator, appendTrigger } from "../core/utils";
+
+export const salePromptOrder = [
+  "prompt_sale",
+  "add_species",
+  "prompt_size",
+  "add_size",
+  "prompt_weight",
+  "add_weight",
+  "prompt_value",
+  "add_value",
+  "prompt_saleConfirmation",
+  "prompt_anotherOne"
+];
+
+export const createSalePrompt = () =>
+  [
+    {
+      id: `prompt_sale`,
+      hideInput: true
+    },
+    {
+      id: `add_species`,
+      hideInput: true,
+      waitAction: true,
+      component: <CatchSelector />
+    },
+    {
+      id: `prompt_size`,
+      hideInput: true
+    },
+    {
+      id: `add_size`,
+      user: true,
+      validator: numberValidator
+    },
+    {
+      id: `prompt_weight`,
+      hideInput: true
+    },
+    {
+      id: `add_weight`,
+      user: true
+    },
+    {
+      id: `prompt_value`,
+      hideInput: true
+    },
+    {
+      id: `add_value`,
+      user: true
+    },
+    {
+      id: `prompt_saleConfirmation`,
+      hideInput: true
+      // component: <SaleDetail />
+    }
+  ].map((p: any, i) => appendTrigger(p, i, salePromptOrder));
