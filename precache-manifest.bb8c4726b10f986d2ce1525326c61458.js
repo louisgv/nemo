2669d98825a5c4a72f9e1cc0734128129bf2848b
Karlos Santos <lgvichy@gmail.com>
2020-03-20 11:51:23 -0700

    Updates

---
+++ precache-manifest.bb8c4726b10f986d2ce1525326c61458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572cfbf8d1feb803c0b5487b704ab133",
+    "url": "/nemo/index.html"
+  },
+  {
+    "revision": "59e7929e377239227c99",
+    "url": "/nemo/static/css/main.da3020f0.chunk.css"
+  },
+  {
+    "revision": "fa304bb339fa5a72f3c4",
+    "url": "/nemo/static/js/2.95f9edf0.chunk.js"
+  },
+  {
+    "revision": "f3399e8bc814081841b32cfe1df001bf",
+    "url": "/nemo/static/js/2.95f9edf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59e7929e377239227c99",
+    "url": "/nemo/static/js/main.226db5d1.chunk.js"
+  },
+  {
+    "revision": "6bf5cfee90da516f792e",
+    "url": "/nemo/static/js/runtime-main.05d13329.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nemo/precache-manifest.3fce8f1640092b5a83530d400da8d2e9.js"
+  "/nemo/precache-manifest.bb8c4726b10f986d2ce1525326c61458.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nemo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
