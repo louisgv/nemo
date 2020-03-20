9a0d30dbd4c823ffaac627b2b1a9ad193201f279
Karlos Santos <lgvichy@gmail.com>
2020-03-19 22:09:32 -0700

    Refactor IPFS and update to factory api

--- src/components/CatchReview.tsx
+++ src/components/CatchReview.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { useFormState } from "react-use-form-state";
-import { animated } from "react-spring";
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { useFormState } from 'react-use-form-state'
+import { animated } from 'react-spring'
 
 import {
   useCatchCacheState,
   fao3AMap,
   useProfileState,
   useLanguageState
-} from "../_data";
+} from '../_data'
 
 import {
   LabeledInput,
@@ -17,72 +17,72 @@ import {
   Divider,
   FillButton,
   StyledLabel
-} from "../_theme";
-import api from "../api";
-import { createCatchPayload } from "../api/catch";
-import { useIpfs } from "../hooks/use-ipfs";
+} from '../_theme'
+import api from '../api'
+import { createCatchPayload } from '../api/catch'
+import { useIpfs } from '../hooks/use-ipfs'
 
 // const debug = require("debug")("DappSendInput");
 
 const Container = styled(animated.div)`
   width: 100%;
   word-break: break-word;
-`;
+`
 
 const QuantityItem = styled(ReviewInput)`
   label {
   }
-`;
+`
 
 export const CatchReview = ({ triggerNextStep, steps }: any) => {
-  const { add_catchArea } = steps;
+  const { add_catchArea } = steps
 
-  const { apiUrl } = api.dapp.dappVault;
+  const { apiUrl } = api.dapp.dappVault
 
-  const { ipfs, isIpfsReady, ipfsInitError } = useIpfs();
+  const { getIpfs, ipfsError } = useIpfs(api.dapp.dappVault.ipfsRepo)
 
-  const [catchCache, setCatchCache] = useCatchCacheState();
+  const [catchCache, setCatchCache] = useCatchCacheState()
 
-  const [profile] = useProfileState();
+  const [profile] = useProfileState()
 
-  const [language] = useLanguageState();
+  const [language] = useLanguageState()
 
-  const [disabled, setDisabled] = useState(false);
+  const [disabled, setDisabled] = useState(false)
 
-  const [sendMethod, setSendMethod] = useState("");
+  const [sendMethod, setSendMethod] = useState('')
 
-  const [error] = useState("");
-  const [success, setSuccess] = useState("");
-  const [, setOriginId] = useState("");
-  const [ipfsHash, setIpfsHash] = useState("");
+  const [error] = useState('')
+  const [success, setSuccess] = useState('')
+  const [, setOriginId] = useState('')
+  const [ipfsHash, setIpfsHash] = useState('')
 
-  const { quantityList, ...catchCacheRest } = catchCache;
+  const { quantityList, ...catchCacheRest } = catchCache
 
   const [formState, { text }] = useFormState({
     ...catchCacheRest,
     catchArea: add_catchArea.value
-  });
+  })
 
   const [dappFormState, dappInput] = useFormState({
     apiUrl,
-    price: "0.0200 EOS"
-  });
+    price: '0.0200 EOS'
+  })
   // console.log(formState.values)
 
   // console.log(Object.keys(formState.values))
 
   if (!quantityList) {
-    return <div>Cache Cleaned . . .</div>;
+    return <div>Cache Cleaned . . .</div>
   }
 
   return (
     <Container>
       <StyledColumnForm
         onSubmit={async e => {
-          e.preventDefault();
-          setDisabled(true);
+          e.preventDefault()
+          setDisabled(true)
 
-          const fishCode = fao3AMap[catchCache.fishKey];
+          const fishCode = fao3AMap[catchCache.fishKey]
 
           const epcisDoc = await createCatchPayload({
             ...profile,
@@ -90,7 +90,7 @@ export const CatchReview = ({ triggerNextStep, steps }: any) => {
             catchArea: add_catchArea.value,
             language,
             fishCode
-          });
+          })
 
           const sendToEOS = async () => {
             // Take the epcisDoc and send it to IPFS, grab the resulting hash
@@ -100,30 +100,30 @@ export const CatchReview = ({ triggerNextStep, steps }: any) => {
             // Get the blockhash and blockid, combine and Output the hash to the user
             const catchResult = await api.dapp.sendCatchEvent(
               dappFormState.values,
-              ipfs,
+              await getIpfs(),
               epcisDoc
-            );
+            )
 
-            setIpfsHash(catchResult.ipfsHash);
-            setOriginId(catchResult.originId);
+            setIpfsHash(catchResult.ipfsHash)
+            setOriginId(catchResult.originId)
 
-            setSuccess("sent");
-          };
+            setSuccess('sent')
+          }
 
           const sendPromise = []
 
           switch (sendMethod) {
-            case "both":
+            case 'both':
               sendPromise.push(sendToEOS())
               sendPromise.push(api.freepcis.sendCatchEvent(epcisDoc))
-              break;
-            case "ipfs-eos":
+              break
+            case 'ipfs-eos':
               sendPromise.push(sendToEOS())
-              break;
+              break
             default:
-            case "freepcis":
+            case 'freepcis':
               sendPromise.push(api.freepcis.sendCatchEvent(epcisDoc))
-              break;
+              break
           }
 
           await Promise.all(sendPromise)
@@ -131,11 +131,10 @@ export const CatchReview = ({ triggerNextStep, steps }: any) => {
           setCatchCache({
             ...catchCache,
             sent: true
-          });
+          })
 
-          triggerNextStep();
-        }}
-      >
+          triggerNextStep()
+        }}>
         {Object.keys(formState.values).map(k => (
           <ReviewInput
             key={k}
@@ -161,54 +160,45 @@ export const CatchReview = ({ triggerNextStep, steps }: any) => {
 
         <FillButton
           disabled={disabled || catchCache.sent}
-          onClick={() => setSendMethod("freepcis")}
-        >
+          onClick={() => setSendMethod('freepcis')}>
           Send to FreEPCIS
         </FillButton>
 
         <br />
 
         <LabeledInput
-          label={"API"}
+          label={'API'}
           disabled={disabled}
           required
-          {...dappInput.text("apiUrl")}
-          placeholder={"find an eos api . . ."}
+          {...dappInput.text('apiUrl')}
+          placeholder={'find an eos api . . .'}
         />
 
         <LabeledInput
-          label={"PRICE"}
+          label={'PRICE'}
           disabled={disabled}
           required
-          {...dappInput.text("price")}
-          placeholder={"selling price . . ."}
+          {...dappInput.text('price')}
+          placeholder={'selling price . . .'}
         />
 
-        {isIpfsReady && (
-          <>
-            <FillButton
-              background={'sandybrown'}
-              disabled={disabled || catchCache.sent}
-              onClick={() => setSendMethod("ipfs-eos")}
-            >
-              Send to EOS
-            </FillButton>
-            <FillButton
-              background={'darkorchid'}
-              disabled={disabled || catchCache.sent}
-              onClick={() => setSendMethod("both")}
-            >
-              Send to FreEPCIS and EOS
-            </FillButton>
-          </>
-        )}
+        <FillButton
+          background={'sandybrown'}
+          disabled={disabled || catchCache.sent}
+          onClick={() => setSendMethod('ipfs-eos')}>
+          Send to EOS
+        </FillButton>
+        <FillButton
+          background={'darkorchid'}
+          disabled={disabled || catchCache.sent}
+          onClick={() => setSendMethod('both')}>
+          Send to FreEPCIS and EOS
+        </FillButton>
       </StyledColumnForm>
 
-      <div style={{ color: "red" }}>
+      <div style={{ color: 'red' }}>
         {error.length > 0 && <span>ERROR: {error}</span>}
-        {ipfsInitError && (
-          <span>ERROR: {ipfsInitError.message || ipfsInitError}</span>
-        )}
+        {ipfsError && <span>ERROR: {ipfsError}</span>}
       </div>
       {success.length > 0 && (
         <div>
@@ -216,16 +206,18 @@ export const CatchReview = ({ triggerNextStep, steps }: any) => {
           <a
             href="https://jungle.eosx.io/account/nemoeosmark1?mode=contract&sub=tables"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             https://jungle.eosx.io/account/nemoeosmark1?mode=contract&sub=tables
           </a>
           <Divider />
           <StyledLabel>
-            {" "}
-            You can check the epcis data on IPFS here -{" "}
+            {' '}
+            You can check the epcis data on IPFS here -{' '}
           </StyledLabel>
-          <a href={`https://ipfs.io/ipfs/${ipfsHash}`} target="_blank" rel="noopener noreferrer">
+          <a
+            href={`https://ipfs.io/ipfs/${ipfsHash}`}
+            target="_blank"
+            rel="noopener noreferrer">
             {`https://ipfs.io/ipfs/${ipfsHash}`}
           </a>
           {/* 
@@ -239,5 +231,5 @@ export const CatchReview = ({ triggerNextStep, steps }: any) => {
         </div>
       )}
     </Container>
-  );
-};
+  )
+}
